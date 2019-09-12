a5efbe69c86d1563e4b3be044bf87e6e8b875f07
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-09-12 05:50:56 +0530

    Better one!

--- simple_dropdown_app/lib/main.dart
+++ simple_dropdown_app/lib/main.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:simple_dropdown_app/HomeView.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  List<String> _items = ["Jack", "Pack", "Tack", "Lack", "Mack"];
+  String _initialValue;
+  List<DropdownMenuItem<String>> listOfItems = new List();
+
+  void _whenChanged(String newItem) {
+    setState(() {
+      _initialValue = newItem;
+    });
+  }
+
+  List<DropdownMenuItem<String>> _buildAndGetList(List<String> items) {
+    List<DropdownMenuItem<String>> finalList = List();
+    for (String u in items) {
+      finalList.add(DropdownMenuItem(
+        value: u,
+        child: Text(u),
+      ));
+    }
+    return finalList;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _initialValue = _items[0];
+    listOfItems = _buildAndGetList(_items);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Drop Down Menu App",
-      home: HomeView(),
+      title: "Test",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is to test DropDown Button"),
+        ),
+        body: Center(
+          child: DropdownButton(
+            value: _initialValue,
+            onChanged: _whenChanged,
+            items: listOfItems,
+          ),
+        ),
+      ),
     );
   }
 }
